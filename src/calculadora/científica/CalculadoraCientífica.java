178ae9a0cad306479453728de379a0f2230faaf0
Diego <dacf188@gmail.com>
2026-09-01 23:07:47 -0600

    Calculadora con operaciones básicas terminada

---
+++ src/calculadora/científica/CalculadoraCientífica.java
@@ -0,0 +1,54 @@
+package calculadora.científica;
+
+import java.util.Scanner;
+
+public class CalculadoraCientífica {
+
+    public static void main(String[] args) {
+        int opcion = 0;
+        double num1, num2;
+        //MENÚ DE ELECCIÓN DE USUARIO
+        Scanner scanner = new Scanner(System.in);
+        Operacion operar = null;
+        do {            
+            System.out.println("\n___MENU DE OPERACIONES___");
+            System.out.println("1. Sumar");
+            System.out.println("2. Resta");
+            System.out.println("3. Multiplicacion");
+            System.out.println("4. Division");
+            System.out.println("5. Potencia");
+            System.out.println("6. Raiz");
+            System.out.println("7. Seno");
+            System.out.println("8. Coseno");
+            System.out.println("9. Tangente");
+            System.out.println("10. Salir");
+
+            opcion = scanner.nextInt();
+            operar = null;
+
+            switch (opcion) {
+                case 1 -> operar = new Suma();
+                case 2 -> operar = new Resta();
+                case 3 -> operar = new Multipliacion();
+                case 4 -> operar = new Division();
+                case 5 -> operar = new Potencia();
+                case 6 -> operar = new Raiz();
+                case 7 -> operar = new Seno();
+                case 8 -> operar = new Coseno();
+                case 9 -> operar = new Tangente();
+                case 10-> {
+                    System.out.println("Saliendo del programa");
+                    break;
+                }
+                default ->
+                    System.out.println("ERROR (SELECCIONE UNA OPCION CORRECTA)");
+            }
+            if (operar != null) {
+                operar.ingresar();
+                operar.operar();
+                operar.imprimir();
+            }
+        } while (opcion != 10);
+    }
+    
+}
---
+++ src/calculadora/científica/Operacion.java
@@ -0,0 +1,203 @@
+package calculadora.científica;
+
+import java.util.Scanner;
+
+abstract class Operacion {
+    protected double num1;
+    protected double num2;
+    protected double resultado;
+    protected double resultado2;
+    
+    //CONSTRUCTOR VACÍO
+    public Operacion (){
+        this.num1 = 0;
+        this.num2 = 0;
+    }
+    //CONSTRUCTOR CON PARAMETROS
+    public Operacion (double num1, double num2){
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+    //GETTERS Y SETTERS
+    public double getNum1 (double num1){
+        return num1;
+    }
+    public double getNum2 (double num2){
+        return num2;
+    }
+    public void setNum1 (double num1){
+        this.num1 = num1;
+    }
+    public void setNum2 (double num2){
+        this.num2 = num2;
+    }
+    
+    public void ingresar (){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese el primer dato");
+        num1 = scanner.nextDouble();
+        System.out.println("Ingrese el segundo dato");
+        num2 = scanner.nextDouble();
+    }
+    //MÉTODO ABSTRACTO
+    public abstract void operar ();
+    //MÉTODO CONCRETO QUE AL MISMO TIEMPO SE SOBREESCRIBE PERO SE HEREDA
+    public void imprimir (){
+        System.out.println("El resultado de la operacion es: " + resultado);
+    }
+}
+class Suma extends Operacion {
+    @Override
+    public void ingresar (){
+        super.ingresar();
+    }
+    @Override
+    public void operar() {
+        resultado = num1 + num2;
+    }
+    @Override
+    public void imprimir() {
+        System.out.println("INGRESO A LA OPCION SUMA");
+        super.imprimir();
+    }
+}
+class Resta extends Operacion {
+    @Override
+    public void ingresar() {
+        super.ingresar();
+    }
+    @Override
+    public void operar() {
+        resultado = num1 - num2;
+    }
+    @Override
+    public void imprimir() {
+        System.out.println("INGRESO A LA OPCION RESTA");
+        super.imprimir();
+    }
+}
+
+class Multipliacion extends Operacion {
+    @Override
+    public void ingresar() {
+        super.ingresar();
+    }
+    @Override
+    public void operar() {
+        resultado = num1 * num2;
+    }
+    @Override
+    public void imprimir() {
+        System.out.println("INGRESO A LA OPCION MULTIPLIACION");
+        super.imprimir();
+    }
+}
+class Division extends Operacion {
+    @Override
+    public void ingresar (){
+        super.ingresar();
+    }
+    @Override
+    public void operar() {
+        if (num2 != 0) {
+            resultado = num1 / num2;
+        } else {
+            // NOT A NUMBER, SIGNIFICA QUE NOS AYUDA A REPRESENTAR UN VALOR NO NUMERICO
+            resultado = Double.NaN;
+        }
+    }
+    @Override
+    public void imprimir() {
+        System.out.println("INGRESO A LA OPCION DIVIDIR");
+        if (Double.isNaN(resultado)) {
+            System.out.println("ERROR (NO SE PUEDE DIVIDIR DENTRO DE 0)");
+        } else {
+            super.imprimir();
+        }
+    }
+}
+class Potencia extends Operacion {
+    @Override
+    public void ingresar (){
+        super.ingresar();
+    }
+    @Override
+    public void operar() {
+        resultado = Math.pow(num1, num1);
+        resultado2 = Math.pow(num2, num2);
+    }
+    @Override
+    public void imprimir() {
+        System.out.println("INGRESO A LA OPCION POTENCIAS");
+        super.imprimir();
+        System.out.println("El resultado de la segunda operacion es: " + resultado2);
+    }
+}
+class Raiz extends Operacion {
+    @Override
+    public void ingresar (){
+        super.ingresar();
+    }
+    @Override
+    public void operar() {
+        resultado = Math.sqrt(num1);
+        resultado2 = Math.sqrt(num2);
+    }
+    @Override
+    public void imprimir() {
+        System.out.println("INGRESO A LA OPCION RAIZ");
+        super.imprimir();
+        System.out.println("El resultado de la segunda operacion es: " + resultado2);
+    }
+}
+class Seno extends Operacion {
+    @Override
+    public void ingresar (){
+        super.ingresar();
+    }
+    @Override
+    public void operar() {
+        resultado = Math.sin(Math.toRadians(num1));
+        resultado2 = Math.sin(Math.toRadians(num2));
+    }
+    @Override
+    public void imprimir() {
+        System.out.println("INGRESO A LA OPCION SENO");
+        super.imprimir();
+        System.out.println("El resultado de la segunda operacion es: " + resultado2);
+    }
+}
+class Coseno extends Operacion {
+    @Override
+    public void ingresar (){
+        super.ingresar();
+    }
+    @Override
+    public void operar() {
+        resultado = Math.cos(Math.toRadians(num1));
+        resultado2 = Math.cos(Math.toRadians(num2));
+    }
+    @Override
+    public void imprimir() {
+        System.out.println("INGRESO A LA OPCION COSENO");
+        super.imprimir();
+        System.out.println("El resultado de la segunda operacion es: " + resultado2);
+    }
+}
+class Tangente extends Operacion {
+    @Override
+    public void ingresar (){
+        super.ingresar();
+    }
+    @Override
+    public void operar() {
+        resultado = Math.tan(Math.toRadians(num1));
+        resultado2 = Math.tan(Math.toRadians(num2));
+    }
+    @Override
+    public void imprimir() {
+        System.out.println("INGRESO A LA OPCION TANGENTE");
+        super.imprimir();
+        System.out.println("El resultado de la segunda operacio es: " + resultado2);
+    }
+}
